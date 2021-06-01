f311983a4f5b0d3a42dc1a35e2ed7f9dc401998c
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-06-01 11:44:55 +0100

    (improvement/style)
    update PokemonDetail Activity and its layout

--- app/src/main/java/com/chidi/pokemongo/presentation/view/PokemonDetail.kt
+++ app/src/main/java/com/chidi/pokemongo/presentation/view/PokemonDetail.kt
@@ -2,11 +2,22 @@ package com.chidi.pokemongo.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.chidi.pokemongo.R
+import com.chidi.pokemongo.databinding.ActivityPokemonDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PokemonDetail : AppCompatActivity() {
+
+    private var binding: ActivityPokemonDetailBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokemon_detail)
+        binding = ActivityPokemonDetailBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
